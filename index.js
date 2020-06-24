eee2a5d67c67b063b75d4fcc7f83bc68500d3deb
DarrenDavis875 <59669452+DarrenDavis875@users.noreply.github.com>
2020-06-24 10:51:35 -0500

    adding http requests GET POST PUT

--- index.js
+++ index.js
@@ -3,51 +3,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
-
-let topMovies = [
-    { title: 'Forest Gump',
-      director: 'Robert Zemeckis',
-      releaseDate:'July 6th, 1994'
-    },
-    { title: 'Jaws',
-      director:'Steven Spielberg',
-      releaseDate:'June 20th, 1975'
-    },
-    { title: 'The Godfather',
-      director:'Francis Ford Coppola',
-      releasDate:'March 24th 1972'
-    },
-    {title:'Star Wars',
-     director:'George Lucas',
-     releasDate: 'May 25th 1977'
-    },
-    { title: 'The Outlaw Josey Wales',
-      director: 'Clint Eastwood',
-      releaseDate: 'August 7th, 1976'
-    },
-    { title: 'The Terminator',
-      director: 'James Cameron',
-      releasDate: 'October 26th, 1984',
-    },
-    { title: 'Rocky',
-      director:'John G. Avildsen',
-      releasDate: 'November 21st, 1976',
-    },
-    { title:'The Big Lobowski',
-      director: 'Ethan Cohen, Joel Cohen',
-      releasDate: 'March 6th, 1998',
-    },
-    { title: 'Alien',
-      director: 'Ridley Scott',
-      releasDate: 'May 25th, 1979',
-    },
-    { title: 'Grease',
-      director: 'Randal Kleiser',
-      releasDate: 'June 13th, 1978',
-    },
-
-];
+const uuid = require('uuid');
 
 app.use(morgan('common'));
 
@@ -57,14 +13,70 @@ app.use((err, req, res, next) => {
     res.status(500).send('Oh No! Something broke!');
   });
 
-  // GET requests
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-
   app.get('/', (req, res) => {
     res.send('Welcome to My Movie Club!');
   });
+
+  // GET requests for all movies
+app.get('/movies', (req, res) => {
+    res.json(movies);
+  });
+
+  // GET request for specific movie by title
+  app.get('/movies/:title', (req, res) => {
+    res.json(movies.find((movie) =>
+      { return movie.title === req.params.name }));
+  });
+
+  // GET request for data about Genre by name
+  app.get('/movies/genre/:name', (req, res) => {
+    res.json(movies.find((genre) =>
+      { return genre.name === req.params.name }));
+  });
+
+
+ // GET request for data about director by name
+ app.get('/movies/director/:name', (req, res) => {
+  res.json(movies.find((director) =>
+    { return director.name === req.params.name }));
+});
+
+// POST request to add new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+ // POST request to add movie to user's favorites list by username
+
+  app.post('/users/favorites/:username', (req, res) => {
+    res.json(users.find((user) =>
+      { return user.username === req.params.username }));
+  });
+  // POST request to delete a movie from user's favorites list by username
+  app.delete('/users/:username', (req, res) => {
+    let user = users.find((user) => { return user.username=== req.params.username });
+  
+    if (user) {
+      users= users.filter((obj) => { return obj.username !== req.params.username });
+      res.status(201).send('Student ' + req.params.username + ' was deleted.');
+    }
+  });
+
+
+
+
+
+  
+
+
 
 
 
